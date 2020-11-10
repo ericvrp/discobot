a32b516bfde6882e785017902ebcd52bc50a3607
Eric van Riet Paap <ericvrp@gmail.com>
2020-11-10 11:01:14 +0100

    rateLimit and progress report to stderr

--- scraper/azlyrics.js
+++ scraper/azlyrics.js
@@ -4,9 +4,15 @@ const puppeteer = require("puppeteer"); // https://github.com/puppeteer/puppetee
 // https://search.azlyrics.com/search.php?q=elvis+presley
 const scrapeAzLyricsByArtist = async (artist = "elvis presley") => {
   const searchUrl = `https://search.azlyrics.com/search.php?q=${artist}`;
+  process.stderr.write(`searchUrl: ${searchUrl}\n`);
 
-  browser = await puppeteer.launch(); // global
+  browser = await puppeteer.launch({
+    slowMo: 1 * 1000, // slow down by 1s
+  }); // global
   page = await browser.newPage(); // global
+  // page.on("console", (msg) =>
+  //   process.stderr.write(`PAGE LOG: ${msg.text()}\n`)
+  // );
 
   await page.goto(searchUrl);
   const artistUrl = await page.$eval("td a", (x) => x.href);
@@ -16,6 +22,8 @@ const scrapeAzLyricsByArtist = async (artist = "elvis presley") => {
 };
 
 const scrapeAzLyricsByArtistUrl = async (artistUrl) => {
+  process.stderr.write(`artistUrl: ${artistUrl}\n`);
+
   await page.goto(artistUrl);
   const songUrls = await page.$$eval(".listalbum-item a", (songs) =>
     songs.map((song) => song.href)
@@ -28,7 +36,7 @@ const scrapeAzLyricsByArtistUrl = async (artistUrl) => {
 
 //
 const scrapeAzLyricsBySongUrl = async (songUrl) => {
-  console.log("Song: " + songUrl);
+  process.stderr.write(`songUrl: ${songUrl}\n`);
 
   await page.goto(songUrl);
   const lines = await page.$$eval(".text-center > div", (lines) =>
@@ -43,7 +51,13 @@ const scrapeAzLyricsBySongUrl = async (songUrl) => {
       break;
     }
   }
-  console.log("Lyrics: " + lyrics + "\n");
+
+  if (lyrics) {
+    // console.log(`Song: ${songUrl}`);
+    console.log(`Lyrics: ${lyrics}\n`);
+  } else {
+    process.stderr.write(`Song not found\n`);
+  }
 };
 
 //
